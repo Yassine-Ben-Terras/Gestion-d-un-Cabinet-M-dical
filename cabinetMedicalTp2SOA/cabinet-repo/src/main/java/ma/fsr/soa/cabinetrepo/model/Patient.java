ba5d7d1e0292e3d109e7d38447d95056732d2e9f
jaouhs <jaouhs@gmail.com>
2026-01-05 19:37:31 +0100

    creation de cabinet-repo

---
+++ cabinetMedicalTp2SOA/cabinet-repo/src/main/java/ma/fsr/soa/cabinetrepo/model/Patient.java
@@ -0,0 +1,23 @@
+package ma.fsr.soa.cabinetrepo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+public class Patient {
+    @Id @GeneratedValue
+    private Long id;
+    private String nom;
+    private LocalDate dateNaissance;
+    private String genre;
+    private String telephone;
+}
