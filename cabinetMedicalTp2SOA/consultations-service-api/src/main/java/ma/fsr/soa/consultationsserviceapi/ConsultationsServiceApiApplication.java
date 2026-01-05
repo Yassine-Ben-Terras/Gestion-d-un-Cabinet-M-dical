cf9e5173e9052868966e1b41944e743552f35be8
jaouhs <jaouhs@gmail.com>
2026-01-06 00:52:59 +0100

    Creation de consultations-service-api

---
+++ cabinetMedicalTp2SOA/consultations-service-api/src/main/java/ma/fsr/soa/consultationsserviceapi/ConsultationsServiceApiApplication.java
@@ -0,0 +1,13 @@
+package ma.fsr.soa.consultationsserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationsServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultationsServiceApiApplication.class, args);
+    }
+
+}
---
+++ cabinetMedicalTp2SOA/consultations-service-api/src/test/java/ma/fsr/soa/consultationsserviceapi/ConsultationsServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsr.soa.consultationsserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationsServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
